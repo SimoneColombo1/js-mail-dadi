2284eee96d0584805e6adf1e41ccac220a0d80ae
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-13 23:52:07 +0200

    creato un if else per capire quale numero sia più grande

--- GiocoDeiDadi/JS/script.js
+++ GiocoDeiDadi/JS/script.js
@@ -1,4 +1,12 @@
 const NumUtente= Number.parseInt(Math.random()*7);
 const NumBot= Number.parseInt(Math.random()*7);
 
+if(NumUtente > NumBot){
 
+    console.log('Complimenti hai Vinto!!!!');
+
+}
+
+else {
+    console.log('Hai perso, prova di nuovo'); 
+}
