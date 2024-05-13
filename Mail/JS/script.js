eb6801ac840a2c2b0f9c19473c56a6972b3faa4b
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-13 23:56:31 +0200

    collegato il file js a index html e creata la variabile Mail utente e l'array mail

---
+++ Mail/JS/script.js
@@ -0,0 +1,18 @@
+const MailUtente= prompt(`inserisci la tua mail`);
+const Mail= [
+
+    "pippufrewoufru-9535@yopmail.com ",
+    "sofauffopuja-7452@yopmail.com ",
+    " weucolekuffoi-8451@yopmail.com",
+    " buketrosice-1852@yopmail.com",
+    "deummiveujijoi-8861@yopmail.com ",
+    " zaboziloyei-9203@yopmail.com",
+    "crouppeudosazoi-4617@yopmail.com "
+
+
+
+];
+
+
+
+
