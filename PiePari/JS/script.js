587aef5f65cf55a9c4e0e17f15924e0797b7c111
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-14 00:09:50 +0200

    Ho dichiarato la variabile che permette all'utente di inserire il numero e l'if else per capire se è pari e nel caso lo sia per inserirle nell'array

--- PiePari/JS/script.js
+++ PiePari/JS/script.js
@@ -1,6 +1,13 @@
-let Num = [];
+let Pari = [];
 
 for(i=0; i<7;i++){
+ let Num = Number.parseInt(prompt('inserisci un Numero')); 
 
-    
+ if (Num % 2===0){
+
+    Pari.push(Num);
+ }
+
 }
+
+console.log(Pari);
